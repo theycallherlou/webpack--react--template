822d3ec6f121f28c32bf0394dc839933db8393cb
Adam Robson <adamrayrobson@gmail.com>
2024-09-19 09:57:40 -0700

    update scripts and add configurations as needed to bring template to current & sound

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic'
+      }
+    ]
   ],
+  plugins: ['@babel/plugin-transform-runtime']
 };
